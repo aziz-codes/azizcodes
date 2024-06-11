ad3e6670635b12fcbccf6f72f2981a5c03251fa8
aziz-codes <azizisoffline@gmail.com>
2024-06-12 00:45:04 +0500

    mini sidebar contents

---
+++ constants/lists.ts
@@ -0,0 +1,19 @@
+import { Files, BriefcaseBusiness, Mail, Shapes } from "lucide-react";
+export const smSidebarLists = [
+  {
+    label: "about",
+    icon: Files,
+  },
+  {
+    label: "projects",
+    icon: BriefcaseBusiness,
+  },
+  {
+    label: "contact me",
+    icon: Mail,
+  },
+  {
+    label: "blogs",
+    icon: Shapes,
+  },
+];
