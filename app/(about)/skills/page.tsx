e5293052d503c2d67813e290bbe90f37943490c1
aziz-codes <azizisoffline@gmail.com>
2024-07-21 20:42:22 +0500

    skills route

--- app/(about)/skills/page.tsx
+++ app/(about)/skills/page.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+"use client";
+import React, { useMemo, useState } from "react";
+import { Search } from "lucide-react";
+import { skills } from "@/constants/skills";
 const Skills = () => {
-  return <div>Skills</div>;
+  const [input, setInput] = useState("");
+
+  const filteredSkills = useMemo(() => {
+    return skills.filter((skill) =>
+      skill.toLowerCase().includes(input.toLowerCase())
+    );
+  }, [input]);
+
+  return (
+    <div className="mt-2 flex min-h-screen space-y-1 py-4 px-4 flex-col gap-6">
+      <div className="border flex w-full max-w-lg py-1.5 px-1 space-x-2 rounded-md items-center">
+        <Search className="text-gray-500" />
+        <input
+          type="text"
+          placeholder="Search skills"
+          className="outline-none w-full bg-transparent"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      <div className="flex flex-wrap gap-4 ">
+        {filteredSkills.map((skill) => (
+          <div
+            className="px-2 py-1.5 rounded-full border text-[12px]"
+            key={skill}
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
