45c60199012ebf1178cb788e8df3abb8befb3f5f
aziz-codes <azizisoffline@gmail.com>
2024-07-29 23:12:27 +0500

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-9 shadow-md w-full bg-bgNavbar border-b sticky top-0 ">
+    <div className="h-9 shadow-md w-full bg-bgNavbar border-b sticky top-0 left-0">
       Navbar
     </div>
   );
