98cf4d6a9fea08f8e214f07de937696637cd96f8
aziz-codes <azizisoffline@gmail.com>
2024-07-29 23:12:37 +0500

    filterable skills page

--- app/(about)/skills/page.tsx
+++ app/(about)/skills/page.tsx
@@ -2,6 +2,7 @@
 import React, { useMemo, useState } from "react";
 import { Search } from "lucide-react";
 import { skills } from "@/constants/skills";
+
 const Skills = () => {
   const [input, setInput] = useState("");
 
@@ -12,25 +13,29 @@ const Skills = () => {
   }, [input]);
 
   return (
-    <div className="mt-2 flex min-h-screen space-y-1 py-4 px-4 flex-col gap-6">
-      <div className="border flex w-full max-w-lg py-1.5 px-1 space-x-2 rounded-md items-center">
-        <Search className="text-gray-500" />
-        <input
-          type="text"
-          placeholder="Search skills"
-          className="outline-none w-full bg-transparent"
-          onChange={(e) => setInput(e.target.value)}
-        />
-      </div>
-      <div className="flex flex-wrap gap-4 ">
-        {filteredSkills.map((skill) => (
-          <div
-            className="px-2 py-1.5 rounded-full border text-[12px]"
-            key={skill}
-          >
-            {skill}
+    <div className="mt-2 flex min-h-screen space-y-1 py-4 px-4 justify-center">
+      <div className="w-full max-w-4xl flex flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <div className="border flex w-full max-w-lg py-1.5 px-1 space-x-2 rounded-md items-center">
+            <Search className="text-gray-500" />
+            <input
+              type="text"
+              placeholder="Search skills"
+              className="outline-none w-full bg-transparent"
+              onChange={(e) => setInput(e.target.value)}
+            />
           </div>
-        ))}
+        </div>
+        <div className="flex flex-wrap gap-4 ">
+          {filteredSkills.map((skill) => (
+            <div
+              className="px-2 py-1.5 rounded-full border text-[12px]"
+              key={skill}
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
