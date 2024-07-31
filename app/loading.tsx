fadd0503a91621fce30d6e398e67768e8152267a
aziz-codes <azizisoffline@gmail.com>
2024-08-01 01:21:03 +0500

    loading animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,13 @@
+import { LoaderCircle } from "lucide-react";
 import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-full h-screen bg-red-500 fixed left-0 top-0">
-      The page is being loaded....
+    <div
+      className="w-full h-screen bg-bgMain flex items-center justify-center fixed left-0 top-0"
+      style={{ zIndex: 9999 }}
+    >
+      <LoaderCircle className="w-6 animate-spin" />
     </div>
   );
 };
