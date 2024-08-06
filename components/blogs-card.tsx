ed3ce2899f642e4421ea9b4054203b7ea579751c
aziz-codes <azizisoffline@gmail.com>
2024-08-06 23:16:19 +0500

    blogs card component

--- components/blogs-card.tsx
+++ components/blogs-card.tsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { CalendarDays } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 interface BlogProps {
   id: number;
   title: string;
   desc: string;
   dateCreated: string;
+  thumbnail: string;
 }
 const BlogsCard = ({ blog }: { blog: BlogProps }) => {
   return (
-    <div className="col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 px-3 py-3 rounded-md flex flex-col space-y-3 bg-[#5B595A]">
-      <div className="w-full h-60 rounded-md bg-sky-500"></div>
+    <Link
+      href={`/blogs/${blog.id}`}
+      className="col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 px-3 py-3 rounded-md flex flex-col space-y-3 bg-[#5B595A] group hover:transition-all duration-150 ease-in-out"
+    >
+      <div className="w-full h-60 rounded-md   relative overflow-hidden">
+        <Image
+          src={blog.thumbnail}
+          fill
+          alt="blog-thumbnail"
+          className="object-cover rounded-lg"
+        />
+        <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent transition-all duration-300 ease-in-out opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0" />
+      </div>
       <h4 className="font-semibold text-sm">{blog.title}</h4>
       <p className="line-clamp-3 text-xs">{blog.desc}</p>
-      <span className="italic text-xs text-gray-400">{blog.dateCreated}</span>
-    </div>
+      <div className="flex items-center space-x-2">
+        <CalendarDays className="text-gray-400 w-4 h-4" />
+        <span className="italic text-xs text-gray-400">{blog.dateCreated}</span>
+      </div>
+    </Link>
   );
 };
 
