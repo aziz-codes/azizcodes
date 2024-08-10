a082c12cec2b2688124e5e46024d7c92752ecbf9
aziz-codes <azizisoffline@gmail.com>
2024-08-10 16:00:53 +0500

    git contribution layout

--- components/github/contributions.tsx
+++ components/github/contributions.tsx
@@ -9,7 +9,7 @@ const Contributions = () => {
     <div
       className={`my-3 flex justify-center overflow-x-auto ${
         open
-          ? "max-w-52 sm:max-w-sm md:max-w-2xl"
+          ? "max-w-52 sm:max-w-sm md:max-w-full"
           : "w-56 sm:w-[500px] md:w-full"
       }`}
     >
