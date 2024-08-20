b84bbdaf5d149ff74d2460022e07fbc17c54f535
aziz-codes <azizisoffline@gmail.com>
2024-08-21 01:09:18 +0500

    experiences

--- constants/experience.ts
+++ constants/experience.ts
@@ -4,17 +4,29 @@ export const experience = [
     company: "Swati Technologies",
     description:
       "Building the frontend in React & Next JS with frameworks like MUI, Chakra UI, Ant Design. Management of state across the application using React Context, Redux Toolkit. Intagration of API's including user authentication and authorization.Web Sockets implementation to real time data communication.integration of WEBRTC for peer to peer communication",
-    tenure: "1 Year",
+    tenure: "2023- Present",
     location: "Lahore",
     type: "on site",
+    present: true,
   },
   {
     designation: "React & Wordpress developer",
     company: "Capillin Private Ltd",
     description:
       "Development of react app using MUI and Tailwind CSS, API integration and state management using Redux.code refactoring and optimizing the application using react best practices. Setup, development and deployment of wordpress website including theme and plugin customization and development.Integration fo diff plugins like WP Bakery for development and woocommerce for managing inventory",
-    tenure: "1.5 Years",
+    tenure: "2021 - 2022",
     location: "Male, Maldives",
     type: "remote",
+    present: false,
+  },
+  {
+    designation: "full stack developer",
+    company: "WNWCHS",
+    description:
+      "A comprehensive housing society management website built using JavaScript, PHP, MySQL, and Bootstrap. The platform features user-based authentication with distinct roles for admin and users. The admin panel allows for managing user accounts, plots, payment verifications, and generating notifications. Users can view notifications and add payments, making the platform a complete solution for efficient housing society operations",
+    tenure: "2021 - ",
+    location: "Rawalpindi",
+    type: "remote",
+    present: false,
   },
 ];
