394054c857c797abec5526bf27e389ffb72d9429
aziz-codes <azizisoffline@gmail.com>
2024-08-21 01:05:25 +0500

    new loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,4 +1,6 @@
 import { LoaderCircle } from "lucide-react";
+import { Avatar } from "@/constants/images";
+import Image from "next/image";
 import React from "react";
 
 const Loading = () => {
@@ -7,7 +9,21 @@ const Loading = () => {
       className="w-full h-screen bg-bgMain flex items-center justify-center fixed left-0 top-0"
       style={{ zIndex: 9999 }}
     >
-      <LoaderCircle className="w-6 animate-spin" />
+      <div className="relative flex items-center justify-center">
+        <Image
+          src={Avatar}
+          alt="Loading Avatar"
+          className="rounded-full"
+          width={80}
+          height={80}
+        />
+
+        <LoaderCircle
+          className="absolute w-28 h-28 text-gray-300 animate-spin"
+          strokeWidth={0.5}
+          stroke="#007ACC"
+        />
+      </div>
     </div>
   );
 };
