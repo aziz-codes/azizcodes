8e85fa0aa48cba1ffbf9f85db9cb92d2e8fa1965
aziz-codes <azizisoffline@gmail.com>
2024-09-17 22:06:47 +0500

    fixed command opening and closing issue, including menu ref

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import * as React from "react";
 import { useRouter, usePathname } from "next/navigation";
-
 import { motion } from "framer-motion";
 import { useStore } from "@/context/context-provider";
 import {
@@ -22,18 +21,20 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Avatar as DP } from "@/constants/images";
 
 const Navbar = () => {
-  const { open: sidebarOpen, setOpen: setSidebarOpen } = useStore();
+  const { open: sidebarOpen, setOpen: setSidebarOpen } = useStore(); 
   const [openMenu, setOpneMenu] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const router = useRouter();
   const pathName = usePathname();
   const menuRef = React.useRef<HTMLDivElement>(null);
+  const commandDialogRef = React.useRef<HTMLDivElement>(null);
+  const divRef = React.useRef<HTMLDivElement>(null)
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
         e.preventDefault();
-        setOpen((open) => !open);
+        setOpen((prev) => !prev);
       }
     };
 
@@ -47,16 +48,25 @@ const Navbar = () => {
   };
 
   React.useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        commandDialogRef.current &&
+        !commandDialogRef.current.contains(e.target as Node)
+      ) {
+        setOpen(false); // Close the command dialog when clicking outside
+      }
       if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
-        setOpneMenu(false);
-        e.stopPropagation();
+        setOpneMenu(false); // Close the menu if it's open
+      }
+      if(divRef.current && divRef.current.contains(e.target as Node)){
+        setOpen(true);
       }
     };
-    window.addEventListener("click", handleClick);
-    return () => window.removeEventListener("click", handleClick);
-  }, []);
 
+    window.addEventListener("click", handleClickOutside);
+    return () => window.removeEventListener("click", handleClickOutside);
+  }, []); 
+ 
   return (
     <div className="h-7 shadow-md w-full bg-bgNavbar border-b sticky select-none top-0 left-0 z-50 flex items-center justify-between px-3 gap-5">
       <div className="space-x-1 items-center hidden md:flex">
@@ -66,9 +76,9 @@ const Navbar = () => {
         >
           <AvatarImage src={DP.src} />
           <AvatarFallback>az</AvatarFallback>
-          <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent" />
+          <div className="absolute bottom-0 right-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent" />
         </Avatar>
-        {links.map((link) => (
+        {links.map((link) => ( 
           <Link
             href={`/${link}`}
             className={`text-[10px] xl:text-sm px-2 text-gray-400 py-0.5 rounded-sm hover:bg-[#3f3f3f] ${
@@ -85,16 +95,57 @@ const Navbar = () => {
       <div className="flex items-center justify-center w-full max-w-md">
         <div
           className="flex items-center border px-2 py-[1px] space-x-2 rounded-sm bg-transparent flex-1 border-[#3f3f3f] justify-between cursor-pointer hover:bg-muted hover:border-slate-700 w-full"
-          onClick={() => setOpen(true)}
+          ref={divRef}
+          
         >
           <Search className="w-4 h-4 text-muted-foreground" />
           <p className="text-[10px] text-muted-foreground">
             Press
-            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded px-1.5 font-semibold text-[10px] font-medium text-muted-foreground opacity-100">
+            <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded px-1.5 text-[10px] font-medium text-muted-foreground opacity-100">
               <span className="text-xs">⌘</span>J
             </kbd>
           </p>
-          <div></div>
+          <div>
+            <div ref={commandDialogRef}>
+              <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Explore the portfolio" />
+                <CommandList>
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup heading="Suggestions">
+                    {suggestions.map((suggestion, index) => {
+                      const Icon = suggestion.icon;
+                      return (
+                        <CommandItem
+                          className="!py-1 my-2  cursor-pointer"
+                          key={index}
+                        >
+                          <div
+                            className="flex items-center space-x-2"
+                            onClick={() => handleItemClick(suggestion.path)}
+                          >
+                            <Icon className="!w-4 !h-4" strokeWidth={0.9} />
+                            <span className="text-xs">{suggestion.label}</span>
+                          </div>
+                        </CommandItem>
+                      );
+                    })}
+                  </CommandGroup>
+                  <CommandSeparator />
+                  <CommandGroup heading="Projects">
+                    {projects.map((project, index) => (
+                      <CommandItem className="!py-1 my-2  cursor-pointer" key={index}>
+                        <div
+                          onClick={() => handleItemClick(`/projects/${project.id}`)}
+                        >
+                          <span className="text-xs">{project.title}</span>
+                        </div>
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </CommandDialog>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -121,7 +172,7 @@ const Navbar = () => {
         </div>
         {openMenu && (
           <motion.div
-            className="flex flex-col gap-2 h-auto py-3 rounded-md w-44 absolute top-6 shadow-lg px-3 left-2 bg-bgMain  border-2"
+            className="flex flex-col gap-2 h-auto py-3 rounded-md w-44 absolute top-6 shadow-lg px-3 right-2 bg-bgMain  border-2"
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
@@ -134,7 +185,7 @@ const Navbar = () => {
                   pathName === `/${link}` && "text-sky-500"
                 }`}
                 key={link}
-                onClick={() => setOpneMenu(false)}
+                onClick={() => setOpen(false)}
               >
                 {link}
               </Link>
