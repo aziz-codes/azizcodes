093d6a3f1bbcc933bf0993e791692f94a8c22cdc
aziz-codes <azizisoffline@gmail.com>
2025-05-03 21:42:16 +0500

    tailwindcss upgraded

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,12 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
 };
-
 export default config;
+
+// old colors
+// bgMain: "#1E1E1E",
+//     bgSidebar: "#333333",
+//     bgNavbar: "#323233",
+//     bgRoutes: "#1E1E1E",
